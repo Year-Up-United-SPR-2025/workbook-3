67c52da2e2db2643d89d6af6ea769b96d49ddb75
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-23 14:46:37 -0400

    created a wb3demos project

--- wb3demos/src/main/java/com/pluralsight/Main.java
+++ wb3demos/src/main/java/com/pluralsight/Main.java
@@ -3,6 +3,6 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
     }
 }
