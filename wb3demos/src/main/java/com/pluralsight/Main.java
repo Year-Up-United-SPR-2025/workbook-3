b92545bec4fc5b449f369dba54301b30e8b8d06c
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-23 15:05:35 -0400

    created the list example project

--- wb3demos/src/main/java/com/pluralsight/Main.java
+++ wb3demos/src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,29 @@
 package com.pluralsight;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello world");
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("Mario-RPG's-List.txt");
+
+            Scanner fileScanner = new Scanner(fis);
+
+            String currentLine;
+
+            while (fileScanner.hasNextLine()) {
+                currentLine = fileScanner.nextLine();
+                System.out.println(currentLine);
+            }
+
+            fileScanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("There is an error in the file");
+        }
     }
 }
